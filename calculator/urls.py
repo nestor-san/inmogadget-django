ea53b2ce59d045ce71a6c257af0b2effd5fd2f1b
Nestor Sanchez <mi@minimo.dev>
2020-08-28 05:34:42 +0200

    Precio final html acabado

--- calculator/urls.py
+++ calculator/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 
 from . import views
 
+app_name = 'calculator'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('precio_final/', views.precio_final, name='precio_final'),
+    path('neto_cliente/', views.neto_cliente, name='neto_cliente'),
+    path('capital_inicial/', views.capital_inicial, name='capital_inicial'),
+    path('comision_agencia/', views.comision_agencia, name='comision_agencia'),
+    path('comision_agente/', views.comision_agente, name='comision_agente'),
 ]
--- calculator/views.py
+++ calculator/views.py
@@ -3,3 +3,19 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'calculator/index.html')
+
+def precio_final(request):
+    return render(request, 'calculator/precio_final.html')
+
+def neto_cliente(request):
+    return render(request, 'calculator/neto_cliente.html')
+
+def capital_inicial(request):
+    return render(request, 'calculator/capital_inicial.html')
+
+def comision_agencia(request):
+    return render(request, 'calculator/comision_agencia.html')
+
+def comision_agente(request):
+    return render(request, 'calculator/comision_agente.html')
+
